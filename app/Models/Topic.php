3059b6e71d30d0170c741f0f99855962a57e6cf8
wizardchao <18317119662@139.com>
2018-04-10 14:01:56 +0000

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    // protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
-    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    // protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -50,4 +50,9 @@ class Topic extends Model
    {
        return route('topics.show', array_merge([$this->id, $this->slug], $params));
    }
+
+   public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
