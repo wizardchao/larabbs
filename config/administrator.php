3f457ed07f1bba8bbb9b3be6405ac17d40a946e9
wizardchao <18317119662@139.com>
2018-04-12 16:20:34 +0000

    Admin permissions

--- config/administrator.php
+++ config/administrator.php
@@ -72,7 +72,7 @@ return array(
     'use_dashboard' => false,
 
     // 设置后台主页视图文件，由 `use_dashboard` 选项决定
-    'dashboard_view' => '',
+    'home_page' => 'topics',
 
     // 用来作为后台主页的菜单条目，由 `use_dashboard` 选项决定，菜单指的是 `menu` 选项
     'home_page' => 'users',
@@ -81,7 +81,7 @@ return array(
     'back_to_site_path' => '/',
 
     // 当选项 `permission` 权限检测不通过时，会重定向用户到此处设置的路径
-    'login_path' => 'login',
+     'login_path' => 'permission-denied',
 
     // 允许在登录成功后使用 Session::get('redirect') 将用户重定向到原本想要访问的后台页面
     'login_redirect_key' => 'redirect',
